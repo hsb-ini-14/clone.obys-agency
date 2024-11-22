151228f10011dc82e0a82c22bbae5577e5b6a38f
hsb-ini-14 <bhaduria14ini@gmail.com>
2024-11-22 15:23:57 +0530

    complete structure of the page defined

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function loaderPageAnimation() {
         opacity: 0 
     })
     tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
-        y:100,
+        y:120,
         stagger: 0.2 
     })
 }
